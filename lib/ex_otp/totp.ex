56f76b6c79c7e74d31f0d4b001b02222c6f1e2a4
Shubham Gupta <shubhamg2208@live.com>
2022-05-07 10:58:16 +0800

    Add support to create new Totp object

--- lib/ex_otp/totp.ex
+++ lib/ex_otp/totp.ex
@@ -20,29 +20,38 @@ defmodule ExOtp.Totp do
           base: Base.t()
         }
 
+  @spec new(integer) :: t()
+  def new(interval) do
+    %Totp{interval: interval, base: %Base{}}
+  end
+
+  @spec validate(t()) :: no_return() | t()
   def validate(%Totp{interval: interval}) when not is_integer(interval) do
     raise Errors.InvalidParam, "interval should be an integer"
   end
 
   def validate(totp), do: totp
 
+  @spec at(t(), integer, integer) :: String.t()
   def at(%Totp{base: base, interval: interval}, for_time, counter \\ 0)
       when is_integer(for_time) do
     Base.generate_otp(base, (for_time / interval) |> round() |> Kernel.+(counter))
   end
 
+  @spec now(t()) :: String.t()
   def now(%Totp{} = totp) do
     at(totp, DateTime.utc_now() |> DateTime.to_unix())
   end
 
   # TODO: Change the comparison to be Timing-Attack Safe.
+  @spec valid?(t(), String.t(), nil | Datetime.t(), nil | integer) :: boolean
   def valid?(%Totp{} = totp, otp, for_time \\ DateTime.utc_now(), valid_window \\ 0) do
     if valid_window do
       Enum.any?(-valid_window..valid_window, fn index ->
         otp == at(totp, DateTime.to_unix(for_time), index)
       end)
     else
-        otp == at(totp, DateTime.to_unix(for_time))
+      otp == at(totp, DateTime.to_unix(for_time))
     end
   end
 end
