dca933e409e00484331dddea7135584385ae6471
Shubham Gupta <shubhamg2208@live.com>
2022-05-08 16:40:35 +0800

    Add fields required for publishing to hex.pm

--- mix.exs
+++ mix.exs
@@ -1,10 +1,13 @@
 defmodule ExOtp.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
+  @repo_url "https://github.com/goodhamgupta/ex_otp/"
+
   def project do
     [
       app: :ex_otp,
-      version: "0.0.1",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -14,7 +17,14 @@ defmodule ExOtp.MixProject do
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
-      ]
+      ],
+      # Hex
+      package: package(),
+      description: "OTP(One-Time-Password) Library in Elixir based on PyOTP",
+
+      # Docs
+      name: "ExOtp",
+      docs: docs()
     ]
   end
 
@@ -29,7 +39,23 @@ defmodule ExOtp.MixProject do
   defp deps do
     [
       {:eqrcode, "~> 0.1.10", optional: true},
-      {:excoveralls, "~> 0.10", only: :test}
+      {:excoveralls, "~> 0.10", only: :test},
+      {:ex_doc, ">= 0.19.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @repo_url}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "ExOtp",
+      source_ref: "v#{@version}",
+      source_url: @repo_url
     ]
   end
 end
