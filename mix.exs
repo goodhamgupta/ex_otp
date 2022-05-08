646994e2ed25a9529115b9e7bc422af8149edf67
Shubham Gupta <shubhamg2208@live.com>
2022-05-08 10:20:24 +0800

    Downgrade version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExOtp.MixProject do
   def project do
     [
       app: :ex_otp,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
