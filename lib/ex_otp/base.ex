0c06b68bfdec372e9913cffb67fdbfb23c6cf60a
Omar Almalol <omar.alma3lol@gmail.com>
2023-03-25 14:53:49 +0200

    refactor(base): changes round to floor

--- lib/ex_otp/base.ex
+++ lib/ex_otp/base.ex
@@ -89,8 +89,8 @@ defmodule ExOtp.Base do
     |> bor((one &&& 0xFF) <<< 16)
     |> bor((two &&& 0xFF) <<< 8)
     |> bor(three &&& 0xFF)
-    # Round is required to convert float to integer
-    |> rem(:math.pow(10, digits) |> round())
+    # Floor is required to convert float to integer
+    |> rem(:math.pow(10, digits) |> floor())
   end
 
   # Private API
